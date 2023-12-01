0bb2cb2624d99ce334b76705a878564bf52491bd
shayan418 <jahanzebshayan14@gmail.com>
2023-12-02 03:29:04 +0530

    day1

--- day1/Trebuchet.java
+++ day1/Trebuchet.java
@@ -1,5 +1,30 @@
 package day1;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Trebuchet {
-    
+    public static void main(String[] args) {
+
+        String fileName = System.getProperty("user.dir") + "/day1/input";
+        int ans = 0;
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                StringBuilder sb = new StringBuilder();
+                for(int i=0; i< line.length(); ++i){
+                    if(line.charAt(i) >= '1' && line.charAt(i) <= '9'){
+                        sb.append( line.charAt(i));
+                        sb.append( line.charAt(i));
+                    }
+                }
+                ans += (sb.charAt(0) - 48) * 10 + (sb.charAt(sb.length() - 1) - 48);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(ans);
+    }
 }
